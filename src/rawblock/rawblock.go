fe15e2ff330a208b30663b540b62a1f30c428347
mutalisk <0x08@0x08.net>
2018-07-13 22:34:51 +0800

    init

--- src/rawblock/rawblock.go
+++ src/rawblock/rawblock.go
@@ -2,26 +2,34 @@ package rawblock
 
 import (
 	"github.com/mutalisk999/bitcoin-lib/src/bigint"
+	"os"
+	"sync"
 )
 
 type RawBlockIndex struct {
 	BlockHeight        uint
 	BlockHash          bigint.Uint256
 	RawBlockSize       uint
-	RawBlockFileIndex  uint
+	RawBlockFileTag    uint
 	RawBlockFileOffset uint
 }
 
 type RawBlockIndexManager struct {
+	BlockIndexFileName string
+	BlockIndexFileObj  os.File
+	blockIndexMutex    sync.Mutex
 }
 
 type RawBlock struct {
-	BlockHeight  uint
-	BlockHash    bigint.Uint256
+	BlockHeight    uint
+	BlockHash      bigint.Uint256
 	CompressedType byte
-	RawBlockSize uint
-	RawBlockData []byte
+	RawBlockSize   uint
+	RawBlockData   []byte
 }
 
 type RawBlockManager struct {
+	RawBlockFileTag uint
+	RawBlockFileObj os.File
+	rawBlockMutex   sync.Mutex
 }
